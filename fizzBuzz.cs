03b209b07006a234b880fac1f9be87e1fe2bf213
Paul <paul.ayling@bglgroup.co.uk>
2019-02-08 12:14:42 +0000

    Refactored to use additional methods

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -6,13 +6,28 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Counter();
+        }
+
+        private static void Counter()
+        {
             for (int i = 1; i <= 100; i++)
             {
-                if (i % 15 == 0) { Console.WriteLine("FizzBuzz"); }
-                else if (i % 3 == 0) { Console.WriteLine("Fizz"); }
-                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
-                else { Console.WriteLine(i); }
+                NumberCheck(i);
             }
+        }
+
+        private static void NumberCheck(int number)
+        {
+            if (DivisibleBy(number, 15)) { Console.WriteLine("FizzBuzz"); }
+            else if (DivisibleBy(number, 3)) { Console.WriteLine("Fizz"); }
+            else if (DivisibleBy(number, 5)) { Console.WriteLine("Buzz"); }
+            else { Console.WriteLine(number); }
+        }
+
+        private static bool DivisibleBy(int number, int dividedBy)
+        {
+            return number % dividedBy == 0;
         }
     }
 }
